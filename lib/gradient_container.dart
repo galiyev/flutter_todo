474816b0aba18d633730a6e6e3f817da4ae54b69
galiyev <kairat.galiyev@gmail.com>
2024-05-01 03:00:40 +0500

    47 Lesson / const

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -13,6 +13,10 @@ class GradientContainer extends StatelessWidget {
 
   final List<Color> colors;
 
+  rollDice(){
+
+  }
+
   //GradientContainer({key}):super(key:key);
    @override
   Widget build(BuildContext context) {
@@ -26,7 +30,23 @@ class GradientContainer extends StatelessWidget {
               end: endAlignment,
           )),
       child: Center(
-        child: Image.asset('assets/images/dice-2.png', width: 200,),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/images/dice-2.png', width: 200,),
+            const SizedBox(height: 20,),
+            TextButton(
+                onPressed: rollDice,
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.only(top:0,),
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(
+                    fontSize: 28
+                  ),
+                ),
+                child: const Text('Roll Dice'))
+          ],
+        )
       ),
     );
   }
